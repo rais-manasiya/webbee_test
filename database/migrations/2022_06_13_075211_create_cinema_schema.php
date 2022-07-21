aeb6c9bca4823ff942a0c17d23ebd69126da4d4d
rais.manasiya <rais.manasiya@eduvanz.com>
2022-07-21 21:10:35 +0530

    Migration schema created for Cinema

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Schema;
 class CreateCinemaSchema extends Migration
 {
     /**
-    # Create a migration that creates all tables for the following user stories
+    # Create a migration that creates all creates for the following user stories
 
     For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
     To not introduce additional complexity, please consider only one cinema.
 
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+    Please list the creates that you would create including keys, foreign keys and attributes that are required by the user stories.
 
     ## User Stories
 
@@ -37,7 +37,123 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('Movie', function(Blueprint $create)
+        {
+            $create->integer('movieID')->unsigned()->nullable();
+            $create->text('description');
+            $create->dateTime('duration', $precision = 0);
+            $create->string('language');
+            $create->dateTime('realeseDate');
+            $create->string('country');
+            $create->string('genre');
+            $create->timestamps();
+        });
+
+        
+        Schema::create('City', function($table) {
+            
+            $create->increments('cityID');
+            $create->string('name',64);
+            $create->string('size',64);
+            $create->string('pincode',12);
+        });
+
+        Schema::create('Cinema', function(Blueprint $create)
+        {
+            $create->integer('cinemaID')->unsigned()->nullable();
+            $create->integer('totalCinemaHall');
+            $create->foreign('city_ID')
+                    ->references('cityID')
+                    ->on('City')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
+        Schema::create('CinemaHall', function(Blueprint $create)
+        {
+            $create->integer('cinemaHallID')->unsigned()->nullable();
+            $create->string('name');
+            $create->integer('totalSeats');
+            $create->foreign('cinemaID')
+                    ->references('cinemaID')
+                    ->on('Cinema')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
+
+        Schema::create('Show', function(Blueprint $create)
+        {
+            $create->integer('showID')->unsigned()->nullable();
+            $create->dateTime('startTime');
+            $create->dateTime('endTime');
+            $create->foreign('cinemaHallId')
+                    ->references('cinemaHallId')
+                    ->on('CinemaHall')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $create->foreign('MovieId')
+                    ->references('movieID')
+                    ->on('Movie')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
+        
+        Schema::create('CinemaSeat', function(Blueprint $create)
+        {
+            $create->integer('cinemaSeatID')->unsigned()->nullable();
+            $create->integer('seatNumber');
+            $create->enum('type', ['Orchestra', 'Mezzanine','Balcony']);
+            $create->foreign('cinemaHallID')
+                    ->references('cinemaHallID')
+                    ->on('CinemaHall')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
+        Schema::create('Booking', function(Blueprint $create)
+        {
+            $create->integer('bookingID')->unsigned()->nullable();
+            $create->integer('noOfSeats');
+            $create->dateTime('Timestamp');
+            $create->enum('status', ['booked', 'cancelled']);
+            $create->foreign('userID')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $create->foreign('showID')
+                    ->references('showID')
+                    ->on('Show')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
+
+
+        Schema::create('ShowSeat', function(Blueprint $create)
+        {
+            $create->integer('showSeatID')->unsigned()->nullable();
+            $create->integer('price');
+            $create->enum('status', ['Available', 'Booked']);
+            $create->foreign('cinemaSeatID')
+                    ->references('cinemaSeatID')
+                    ->on('CinemaSeat')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $create->foreign('showID')
+                    ->references('showID')
+                    ->on('Show')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $create->foreign('bookingID')
+                    ->references('bookingID')
+                    ->on('Booking')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+
     }
 
     /**
@@ -47,5 +163,15 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('Movie');
+        Schema::dropIfExists('Show');
+        Schema::dropIfExists('Boking');
+        Schema::dropIfExists('Cinema');
+        Schema::dropIfExists('CinemaHall');
+        Schema::dropIfExists('City');
+        Schema::dropIfExists('CinemaSeat');
+        Schema::dropIfExists('ShowSeat');
+
+
     }
 }
